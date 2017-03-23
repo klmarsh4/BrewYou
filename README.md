b4d72f3091cfc43fd4353863f05b8408c043408d
Kevin Marsh <klmarsh4@gmail.com>
2017-03-23 18:52:33 -0400

    Created users package

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BrewYou
 This app will locate nearby breweries.
+
+until I figure out how to manage packages, run this command to install mongo library:
+go get gopkg.in/mgo.v2
--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
     "fmt"
-    "gopkg.in/mgo.v2"
+    "BrewYou/users"
 )
 
 func main() {
-  session, _ := mgo.Dial("mongodb://<username>:<password>@ds147985.mlab.com:47985/brewyou")
-  //fmt.Println(err)
-  c := session.DB("brewyou").C("Users")
-  var result interface{}
-  c.Find(nil).One(&result)
-  fmt.Println(result)
+  user := users.GetOne()
+  fmt.Println(user)
 }
